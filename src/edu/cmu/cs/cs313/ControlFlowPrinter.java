81b97acca4b26e32c48fedae912c7fc4581cc967
Miguel Velez <miguelvelez@mijecu25.com>
2018-10-29 20:38:47 -0400

    added task

--- src/edu/cmu/cs/cs313/ControlFlowPrinter.java
+++ src/edu/cmu/cs/cs313/ControlFlowPrinter.java
@@ -32,17 +32,19 @@ public class ControlFlowPrinter extends CFGDetector{
 	}
 
 	private void printCFG(CFG cfg){
-		int count = 0;
 		HashMap<BasicBlock,Integer> blockRename = new HashMap<BasicBlock,Integer>();
 		Iterator<BasicBlock> bbIter = cfg.blockIterator();
-		//iterate throught the blocks in the control flow graph
+		//iterate through the blocks in the control flow graph
+		// note that this simply iterates over the list of blocks provided by the cfg;
+		// it does not follow paths in any interesting way.
 		while(bbIter.hasNext()){
-			//print the indentification information for each block
+			//print the identification information for each block
 			BasicBlock currentBlock = bbIter.next();
 			if(currentBlock.getLabel()==cfg.getEntry().getLabel()){
 				System.out.println("Starting Block!");
 			}
 			System.out.println("Current Block Label: "+currentBlock.getLabel());
+			blockRename.put(currentBlock, currentBlock.getLabel());
 			//print out the java byte code instructions in each block
 			System.out.println("instructions for block "+blockRename.get(currentBlock)+":");
 			InstructionIterator ii = currentBlock.instructionIterator();
@@ -52,7 +54,7 @@ public class ControlFlowPrinter extends CFGDetector{
 			}
 			//print out the list of blocks which come before this block in the control flow graph
 			Iterator<BasicBlock> predIterator = cfg.predecessorIterator(currentBlock);
-			System.out.print("predecessors: ");
+			System.out.print("predecessor blocks: ");
 			while(predIterator.hasNext()){
 				int blockName = predIterator.next().getLabel();
 				System.out.print(blockName+", ");
@@ -60,7 +62,7 @@ public class ControlFlowPrinter extends CFGDetector{
 			System.out.println(""); //finished the current line
 			//print out the list of blocks which come after this block in the control flow graph
 			Iterator<BasicBlock> successorIterator = cfg.successorIterator(currentBlock);
-			System.out.print("successors: ");
+			System.out.print("successor blocks: ");
 			while(successorIterator.hasNext()){
 				int blockName = successorIterator.next().getLabel();
 				System.out.print(blockName+", ");
---
+++ src/edu/cmu/cs/cs313/CycleDetector.java
@@ -0,0 +1,101 @@
+/*
+ * FindBugs - Find Bugs in Java programs
+ * Copyright (C) 2003-2008 University of Maryland
+ * 
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ * 
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+package edu.cmu.cs.cs313;
+
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
+
+import edu.umd.cs.findbugs.BugInstance;
+import edu.umd.cs.findbugs.BugReporter;
+import edu.umd.cs.findbugs.ba.BasicBlock;
+import edu.umd.cs.findbugs.ba.CFG;
+import edu.umd.cs.findbugs.ba.Edge;
+import edu.umd.cs.findbugs.ba.EdgeTypes;
+import edu.umd.cs.findbugs.bcel.CFGDetector;
+import edu.umd.cs.findbugs.classfile.CheckedAnalysisException;
+import edu.umd.cs.findbugs.classfile.MethodDescriptor;
+
+/**
+ * This detector will issue an error if there is a cycle in the control flow graph.
+ */
+public class CycleDetector extends CFGDetector {
+
+	private final BugReporter bugReporter;
+
+	public CycleDetector(BugReporter bugReporter) {
+		this.bugReporter = bugReporter;
+		System.out.println("Loading CycleDetector detector");
+	}
+
+	/**
+	 * by FindBugs for every method in the system; contains the actual analysis
+	 */
+	protected void visitMethodCFG(MethodDescriptor methodDescriptor, CFG cfg)
+			throws CheckedAnalysisException {
+
+		if (hasCycle(cfg)) {
+			System.err.println("Found cycle in method "
+					+ methodDescriptor.getName());
+			bugReporter.reportBug(new BugInstance(this,
+					"CS_RAND_BEFORE_SIN", HIGH_PRIORITY).addClassAndMethod(
+					methodDescriptor));
+		}
+
+	}
+
+	/**
+	 * search for a cycle in the CFG between any node
+	 * 
+	 * @param cfg
+	 *            the graph
+	 * @return true if there is a path, false otherwise
+	 */
+	private boolean hasCycle(CFG cfg) {
+		Set<BasicBlock> blocks = new HashSet<>();
+		blocks.add(cfg.getEntry());
+		Set<BasicBlock> visited = new HashSet<>();
+		
+		while (!blocks.isEmpty()) {
+			Iterator<BasicBlock> it = blocks.iterator();
+			BasicBlock block = it.next();
+			it.remove();
+			
+			if(visited.contains(block)) {
+				return true;
+			}
+			
+			visited.add(block);
+			Iterator<Edge> childrenIterator = cfg.outgoingEdgeIterator(block);
+			
+            while (childrenIterator.hasNext()) {
+                Edge edge = childrenIterator.next();
+                // TODO: The control flow graph has unhandled exceptional edges, which might
+                // produce false positives of a cycle. You need to filter a specific kind of
+                // edge that should not be added to the blocks to analyze.
+                BasicBlock child = edge.getTarget();
+                blocks.add(child);
+            }
+
+		}
+		return false;
+	}
+	
+}
